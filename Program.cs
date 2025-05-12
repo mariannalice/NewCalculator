7c3865474adc55d3fc31e9c495bf994b0e54d54f
wam0824 <wam082024@gmail.com>
2025-05-12 12:15:39 +0200

    basic calculator

--- Program.cs
+++ Program.cs
@@ -1,2 +1,43 @@
-﻿using NewCalculator.Operators;
+﻿using System.Collections;
+using NewCalculator.Operators;
 
+class Program
+{
+  static void Main ()
+  {
+    Console.WriteLine("Welcome to your New Calculator!");
+    //Console.WriteLine("Enter two numbers and one of the following operators: | + | - | * | / |");
+    Console.WriteLine("Enter you first number: ");
+    string? FirstInput = Console.ReadLine();
+    double FirstNumber = double.Parse(FirstInput);
+
+    Console.WriteLine("Enter you second number: ");
+    string SecondInput = Console.ReadLine();
+    double SecondNumber = double.Parse(SecondInput);
+
+    Console.WriteLine("Enter a operator: ");
+    string? Operator = Console.ReadLine();
+
+    double result = 0;
+    switch (Operator)
+    {
+      case "+":
+      result = Add.Addition(FirstNumber, SecondNumber);
+      break;
+
+      case "-":
+      result = Subtract.Subtraction(FirstNumber, SecondNumber);
+      break;
+
+      case "*":
+      result = Multiply.Multiplication(FirstNumber, SecondNumber);
+      break;
+
+      case "/":
+      result = Divide.Division(FirstNumber, SecondNumber);
+      break;
+    }
+
+    Console.WriteLine($"{FirstNumber} {Operator} {SecondNumber} = {result}");
+  }
+}
